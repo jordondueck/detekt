9e7f275621ca66bdb86433439950734d94137120
Jordon Dueck <jordon.dueck@gmail.com>
2020-01-18 12:52:11 -0800

    Add Sign In and Registration components

---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Registration.css";
+
+const Registration = () => {
+  return (
+    <div className="registration-container">
+      
+    </div>
+  );
+};
+
+export default Registration;
---
+++ src/components/SignIn/SignIn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SignIn.css";
+
+const SignIn = () => {
+  return (
+    <div className="signin-container">
+      
+    </div>
+  );
+};
+
+export default SignIn;
