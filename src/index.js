5f30c92d19baf409898e0f2abd820e64b80a151d
Jordon Dueck <jordon.dueck@gmail.com>
2020-01-29 04:37:19 -0800

    Register serviceWorker to allow app to work in Firefox (otherwise receive SecurityError)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
+// If you want your app to work in the Firefox browser, work offline and
+// load faster, you can change unregister() to register() below.
+// Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+serviceWorker.register();
