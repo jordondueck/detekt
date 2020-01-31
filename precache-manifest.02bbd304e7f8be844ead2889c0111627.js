682ffc3cd700982812b7cb108ea126905898094c
Jordon Dueck <jordon.dueck@gmail.com>
2020-01-30 22:31:50 -0800

    Updates

---
+++ precache-manifest.02bbd304e7f8be844ead2889c0111627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25287e73a68397be9363f8d4bd030630",
+    "url": "/detekt/index.html"
+  },
+  {
+    "revision": "7c4e3f4a97f4ef2a39c6",
+    "url": "/detekt/static/css/main.58c2f1af.chunk.css"
+  },
+  {
+    "revision": "810a895afedb7dd3c03f",
+    "url": "/detekt/static/js/2.3cea5a11.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/detekt/static/js/2.3cea5a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4e3f4a97f4ef2a39c6",
+    "url": "/detekt/static/js/main.1c5539f5.chunk.js"
+  },
+  {
+    "revision": "b5ec48ab4d10024016b9",
+    "url": "/detekt/static/js/runtime-main.6f641797.js"
+  },
+  {
+    "revision": "63d10e19ceba35ba5b445943739c204a",
+    "url": "/detekt/static/media/logo.63d10e19.png"
+  }
+]);
