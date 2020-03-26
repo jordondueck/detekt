f0c5b85f3c40e9dd25366bbb418fed61ca335098
Jordon Dueck <jordon.dueck@gmail.com>
2020-03-25 20:49:28 -0700

    Fix navbar layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./Navigation.css";
 import Logo from "../Logo/Logo";
+import {
+  Button,
+  FormControl,
+  FormGroup
+  // FormLabel
+} from "react-bootstrap";
 
 const Navigation = ({ route, isSignedIn, handleRouteChange }) => {
   return (
-    <nav className="nav">
-      <div className="nav-section">
-        <Logo />
-      </div>
-      <ul className="nav-section nav-title">
-        <li>
-          {/* <h1 className="title title--big">detekt</h1> */}
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-section">
+          <Logo />
         </li>
-        <li>
-          {/* <h3 className="title title--small">Facial Recognition System</h3> */}
-        </li>
-      </ul>
-      {route === "signin" ? (
-        <div className="nav-section">
-          <p
+        {/* <li className="nav-section>">
+          Detekt
+        </li> */}
+        {route === "signin" ? (
+        <li className="nav-section">
+          <Button
             className="nav-item"
+            variant="outline-dark"
             onClick={() => handleRouteChange("registration")}
           >
             Register
-          </p>
-        </div>
+          </Button>
+        </li>
       ) : route === "registration" ? (
         <div className="nav-section">
           <p className="nav-item" onClick={() => handleRouteChange("signin")}>
@@ -40,6 +43,45 @@ const Navigation = ({ route, isSignedIn, handleRouteChange }) => {
       ) : (
         undefined
       )}
+      </ul>
+
+
+      {/* <div className="nav-section">
+        <Logo />
+      </div>
+      <ul className="nav-section nav-title">
+        <li>
+          <h1 className="title title--big">detekt</h1>
+        </li>
+        <li>
+          <h3 className="title title--small">Facial Recognition System</h3>
+        </li>
+      </ul>
+      {route === "signin" ? (
+        <div className="nav-section">
+          <Button
+            className="nav-item"
+            variant="outline-dark"
+            onClick={() => handleRouteChange("registration")}
+          >
+            Register
+          </Button>
+        </div>
+      ) : route === "registration" ? (
+        <div className="nav-section">
+          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
+            Sign In
+          </p>
+        </div>
+      ) : isSignedIn ? (
+        <div className="nav-section">
+          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
+            Sign Out
+          </p>
+        </div>
+      ) : (
+        undefined
+      )} */}
     </nav>
   );
 };
