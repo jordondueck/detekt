ba6cb85bfac7147da66ca0de4a638f6f80bc5ce6
Jordon Dueck <jordon.dueck@gmail.com>
2020-03-28 15:29:52 -0700

    Improve image input appearance

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,24 +9,24 @@ import {
 
 const ImageLinkForm = ({ handleInputChange, handleButtonSubmit }) => {
   return (
-    <form className="form form--wide form--top">
+    <form className="form form--wide">
       <h3 className="title title--small">{"Enter Image URL"}</h3>
-      {/* <div className="form__content"> */}
-      <FormGroup className="form-group--wide form-group--inline" controlId="imageurl">
+      <section className="form__content">
+      <FormGroup className="form-group--wide form-group--inline form-group--no-msg" controlId="imageurl">
         <FormControl
           type="text"
           placeholder="https://example.com/images/example.jpg"
           onChange={handleInputChange}
         />
         <Button
-          className="button--ml"
+          className="button--ml button--no-outline"
           variant="outline-dark"
           onClick={handleButtonSubmit}
         >
           {"Detect"}
         </Button>
       </FormGroup>
-      {/* </div> */}
+      </section>
       {/* <p className="text__tip text--small">
         Note: Large images take longer to process.
       </p> */}
