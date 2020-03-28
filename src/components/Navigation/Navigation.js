e8976ad3b6b74d20abc66d37b1c5b2f24947a409
Jordon Dueck <jordon.dueck@gmail.com>
2020-03-28 01:21:25 -0700

    Refactor navbar CSS

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -12,76 +12,40 @@ const Navigation = ({ route, isSignedIn, handleRouteChange }) => {
   return (
     <nav>
       <ul className="nav-list">
-        <li className="nav-section">
+        <li>
           <Logo />
         </li>
-        {/* <li className="nav-section>">
-          Detekt
-        </li> */}
         {route === "signin" ? (
-        <li className="nav-section">
-          <button
-            className="nav-item button--text"
-            // variant="outline-dark"
-            onClick={() => handleRouteChange("registration")}
-          >
-            Register
-          </button>
-        </li>
-      ) : route === "registration" ? (
-        <div className="nav-section">
-          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
-            Sign In
-          </p>
-        </div>
-      ) : isSignedIn ? (
-        <div className="nav-section">
-          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
-            Sign Out
-          </p>
-        </div>
-      ) : (
-        undefined
-      )}
+          <li>
+            <button
+              className="nav-item button--text"
+              onClick={() => handleRouteChange("registration")}
+            >
+              Register
+            </button>
+          </li>
+        ) : route === "registration" ? (
+          <li>
+            <button
+              className="nav-item button--text"
+              onClick={() => handleRouteChange("signin")}
+            >
+              Sign In
+            </button>
+          </li>
+        ) : isSignedIn ? (
+          <li>
+            <button
+              className="nav-item button--text"
+              onClick={() => handleRouteChange("signin")}
+            >
+              Sign Out
+            </button>
+          </li>
+        ) : (
+          undefined
+        )}
       </ul>
-
-
-      {/* <div className="nav-section">
-        <Logo />
-      </div>
-      <ul className="nav-section nav-title">
-        <li>
-          <h1 className="title title--big">detekt</h1>
-        </li>
-        <li>
-          <h3 className="title title--small">Facial Recognition System</h3>
-        </li>
-      </ul>
-      {route === "signin" ? (
-        <div className="nav-section">
-          <Button
-            className="nav-item"
-            variant="outline-dark"
-            onClick={() => handleRouteChange("registration")}
-          >
-            Register
-          </Button>
-        </div>
-      ) : route === "registration" ? (
-        <div className="nav-section">
-          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
-            Sign In
-          </p>
-        </div>
-      ) : isSignedIn ? (
-        <div className="nav-section">
-          <p className="nav-item" onClick={() => handleRouteChange("signin")}>
-            Sign Out
-          </p>
-        </div>
-      ) : (
-        undefined
-      )} */}
     </nav>
   );
 };
