2c3de1fbbf5ec1bdad88f081b717f2913eb2e3a5
Jordon Dueck <jordon.dueck@gmail.com>
2020-04-01 15:50:08 -0700

    Refactor CSS

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,35 +1,30 @@
 import React from "react";
 import "./ImageLinkForm.css";
-import {
-  Button,
-  FormControl,
-  FormGroup
-  // FormLabel
-} from "react-bootstrap";
+import { Button, FormControl, FormGroup } from "react-bootstrap";
 
 const ImageLinkForm = ({ handleInputChange, handleButtonSubmit }) => {
   return (
     <form className="form form--wide">
       <h3 className="title title--small">{"Enter Image URL"}</h3>
       <section className="form__content">
-      <FormGroup className="form-group--wide form-group--inline form-group--no-msg" controlId="imageurl">
-        <FormControl
-          type="text"
-          placeholder="https://example.com/images/example.jpg"
-          onChange={handleInputChange}
-        />
-        <Button
-          className="button--ml button--no-outline"
-          variant="outline-dark"
-          onClick={handleButtonSubmit}
+        <FormGroup
+          className="form-group--wide form-group--inline form-group--no-msg"
+          controlId="imageurl"
         >
-          {"Detect"}
-        </Button>
-      </FormGroup>
+          <FormControl
+            type="text"
+            placeholder="https://example.com/images/example.jpg"
+            onChange={handleInputChange}
+          />
+          <Button
+            className="button--ml button--no-outline"
+            variant="outline-dark"
+            onClick={handleButtonSubmit}
+          >
+            {"Detect"}
+          </Button>
+        </FormGroup>
       </section>
-      {/* <p className="text__tip text--small">
-        Note: Large images take longer to process.
-      </p> */}
     </form>
   );
 };
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,18 +1,10 @@
 import React, { Fragment } from "react";
-import "./Logo.css";
-// import Tilt from "react-tilt";
 import logo from "./logo.png";
 
 const Logo = () => {
   return (
     <Fragment>
-    {/* // <div className="logo"> */}
-      {/* <Tilt options={{ max: 50 }}> */}
-        {/* <div> */}
-          <img className="logo-img" src={logo} alt="Logo" />
-        {/* </div> */}
-      {/* </Tilt> */}
-    {/* // </div> */}
+      <img className="logo-img" src={logo} alt="Logo" />
     </Fragment>
   );
 };
