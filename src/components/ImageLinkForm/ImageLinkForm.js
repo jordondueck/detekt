4c727f3159159274f172f1ae2a0e51314cbc1eec
Jordon Dueck <jordon.dueck@gmail.com>
2020-04-13 00:09:13 -0700

    Add undo button inside user input to clear loaded image before detecting faces

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,7 +9,7 @@ const ImageLinkForm = ({
   handleButtonSubmit,
   inputUrl,
   detectSelected,
-  handleButtonReset
+  handleButtonReset,
 }) => {
   return (
     <Formik
@@ -24,34 +24,71 @@ const ImageLinkForm = ({
         handleInputChange(values.imageUrl);
         handleButtonReset();
       }}
+      validateOnChange={false}
+      validateOnBlur={false}
     >
-      {({ isSubmitting }) => (
+      {({ isSubmitting, handleReset }) => (
         <Form className="form form--wide">
           <div className="form__content">
             <h2>Facial Recognition System</h2>
             <div className="form__inner">
-            <FormGroup
-              className="form-group--wide form-group--inline"
-              controlId="imageurl"
-            >
-              <Field name="imageUrl">
-                {({ field, meta }) => (
-                  <FormControl
-                    {...field}
-                    className={meta.error && meta.touched ? "input--error" : ""}
-                    type="text"
-                    placeholder="https://example.com/images/example.jpg"
-                  />
+              <FormGroup
+                className="form-group--wide form-group--inline"
+                controlId="imageurl"
+              >
+                {inputUrl === "" ? (
+                  <div className="input_container form-control">
+                    <Field name="imageUrl">
+                      {({ field, meta }) => (
+                        <FormControl
+                          {...field}
+                          className={
+                            meta.error && meta.touched
+                              ? "input--error form-control--clear"
+                              : "form-control--clear"
+                          }
+                          type="text"
+                          placeholder="https://example.com/images/example.jpg"
+                        />
+                      )}
+                    </Field>
+                  </div>
+                ) : (
+                  <div className="input_container input_container--grey form-control">
+                    <Field name="imageUrl input_input">
+                      {({ field, meta }) => (
+                        <FormControl
+                          {...field}
+                          className={
+                            meta.error && meta.touched
+                              ? "input--error form-control--clear pr--75"
+                              : "form-control--clear pr--75"
+                          }
+                          type="text"
+                          placeholder="https://example.com/images/example.jpg"
+                          readOnly
+                        />
+                      )}
+                    </Field>
+                    <i
+                      className="fa fa-undo button--undo"
+                      aria-hidden="true"
+                      onClick={handleReset}
+                    ></i>
+                  </div>
                 )}
-              </Field>
-              <div className="error__wrapper">
-              <ErrorMessage className="error" name="imageUrl" component="span" />
-              </div>
+                <div className="error__wrapper">
+                  <ErrorMessage
+                    className="error"
+                    name="imageUrl"
+                    component="span"
+                  />
+                </div>
               </FormGroup>
               {detectSelected ? (
                 <Button
                   className="button--ml button--no-outline"
-                  variant="outline-dark"
+                  variant="danger"
                   type="reset"
                 >
                   Clear
@@ -59,41 +96,17 @@ const ImageLinkForm = ({
               ) : (
                 <Button
                   className="button--ml button--no-outline"
-                  variant="outline-dark"
+                  variant={inputUrl === "" ? "primary" : "success"}
                   type="submit"
                 >
                   {inputUrl === "" ? "Load" : "Detect"}
                 </Button>
               )}
-              </div>
-            
+            </div>
           </div>
         </Form>
       )}
     </Formik>
-
-    // <form className="form form--wide">
-    //   <div className="form__content">
-    //   <h2>{"Facial Recognition System"}</h2>
-    //     <FormGroup
-    //       className="form-group--wide form-group--inline form-group--no-msg"
-    //       controlId="imageurl"
-    //     >
-    //       <FormControl
-    //         type="text"
-    //         placeholder="https://example.com/images/example.jpg"
-    //         onChange={handleInputChange}
-    //       />
-    //       <Button
-    //         className="button--ml button--no-outline"
-    //         variant="outline-dark"
-    //         onClick={handleButtonSubmit}
-    //       >
-    //         {"Detect"}
-    //       </Button>
-    //     </FormGroup>
-    //   </div>
-    // </form>
   );
 };
 
